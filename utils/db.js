cce03a8be0f344bad5d16eb3c028a84e5bc93fb3
Imanol Bernal <info@ibernal.dev>
2022-02-22 18:11:44 +0100

    Vercel test

--- utils/db.js
+++ utils/db.js
@@ -9,13 +9,22 @@ export const db = mysql({
     port: parseInt(process.env.MYSQL_PORT)
   }
 })
+console.log({
+  host: process.env.MYSQL_HOST,
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  port: parseInt(process.env.MYSQL_PORT)
+})
 
 export async function query (q, values) {
   try {
     const results = await db.query(q, values)
     await db.end()
+    console.log(results)
     return results
   } catch (e) {
+    console.log(e)
     throw Error(e.message)
   }
 }
